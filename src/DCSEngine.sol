bea2efe7ca8eb715eb8eca6d1e8936fcc49dfd92
Walter Nwaugo <47696744+walterthesmart@users.noreply.github.com>
2024-04-22 12:21:39 +0100

    Decentralized stable coin

---
+++ src/DCSEngine.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+
+/**
+ * @title DSCEngine
+ * @author Nwaugo Walter
+ * @notice 
+ */
+contract DSCEngine {}
--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -12,4 +12,37 @@ pragma solidity 0.8.24;
  * 
  * This is the contract meant to be governed by DCSEngine. This contract is justthe ERC20 implementation of our stablecoin system
  */
-contract DecentralizedStableCoin {}
+
+import {ERC20, ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract DecentralizedStableCoin is ERC20Burnable, Ownable {
+    error DecentralizedStableCoin_MustBeMoreThanZero();
+    error DecentralizedStableCoin_BurnAmountExceedsBalance();
+    error DecentralizedStableCoin_NotZeroAddress();
+
+    constructor() ERC20("Decentralized Stable Coin", "DSC") Ownable(msg.sender) {}
+
+    function burn(uint256 _amount) public override onlyOwner {
+        uint256 balance = balanceOf(msg.sender);
+        if(_amount <= 0){
+            revert DecentralizedStableCoin_MustBeMoreThanZero();
+        }
+        if(balance < _amount){
+            revert DecentralizedStableCoin_BurnAmountExceedsBalance();
+        }
+        super.burn(_amount);
+    }
+
+    function mint(address _to, uint256 _amount) external onlyOwner returns (bool) {
+        if(_to == address(0)){
+            revert DecentralizedStableCoin_NotZeroAddress();
+        }
+        if(_amount <= 0){
+            revert DecentralizedStableCoin_MustBeMoreThanZero();
+        }
+        super._mint(_to, _amount);
+        return true;
+    }
+
+}
